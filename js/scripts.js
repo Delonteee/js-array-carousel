62c62c07fd4f7d1abf9249ff556a48d9dcbce47b
Delonteee <davide.gasparotti@tutanota.com>
2023-12-13 17:32:14 +0100

    Esercizio completo

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+
+
+/* Inseriamo le singole img in delle variabili con un template Literal con il quale 
+integriamo la variabile della classe */
+
+const img1 =  document.querySelector('.canvas').innerHTML = `<img src="img/01.webp">`;
+const img2 =  document.querySelector('.canvas').innerHTML = `<img src="img/02.webp">`;
+const img3 =  document.querySelector('.canvas').innerHTML = `<img src="img/03.webp">`;
+const img4 =  document.querySelector('.canvas').innerHTML = `<img src="img/04.webp">`;
+const img5 =  document.querySelector('.canvas').innerHTML = `<img src="img/05.webp">`;
+
+// array delle img da inserire
+const imgArray = [ 
+     ` ${img1} ` ,
+     ` ${img2} ` ,
+     ` ${img3} ` ,
+     ` ${img4} ` ,
+     ` ${img5} ` 
+    ];
+      
+    
+document.querySelector('.canvas').innerHTML = (imgArray[0]);
+
+let counter = 0 ;
+
+// associamo una variabile ai bottoni
+const buttonRight = document.getElementById('right-button');
+const buttonLeft = document.getElementById('left-button');
+
+buttonRight.addEventListener('click', function(){
+    if (counter < imgArray.length -1){
+        counter += 1;
+        console.log(counter);
+        document.querySelector('.canvas').innerHTML = imgArray[counter];
+    }
+})
+
+buttonLeft.addEventListener('click', function(){
+        if ((counter !== imgArray.length) && (counter !== 0)){
+            counter -= 1
+            console.log(counter)
+            document.querySelector('.canvas').innerHTML = (imgArray[counter])
+        }
+}) 
